f4ba5b94bcbd8889b68fd7128352ee4b5a8228b3
Ivan1507 <iancurtis1507@mail.ru>
2020-09-13 19:12:47 +0300

    Learn IT

--- src/animal.java
+++ src/animal.java
@@ -1,26 +1,84 @@
+
+/**
+ * Класс продукции со свойствами <b>maker</b> и <b>price</b>.
+ * @autor 123
+ * @version 2.1
+ */
+import java.awt.*;
+import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Set;
 
-public class animal {
+public class animal extends IOException {
+    public String Name;
+
     public static void main(String[] args){
         int left=1;
         int right=100;
-        while(left<right) {
-            int middle=(left+right)/2;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Your number > "+middle+"?"+"(1/0)");
-            int q = sc.nextInt();
-            if(q==1)
-                left=middle+1;
-            else if(q==0)
-                right=middle;
-            else System.out.println("Enter 1 or 0");
+
+        int[] mas={45,678,123,89,1,107};
+        Properties pr=new Properties();
+        pr.setProperty("Hello","Privet");
+        Arrays.sort(mas);
+        animal an=new animal();
+        an.Name="Vasya";
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+
+        double m=0;
+        double sm=0;
+        boolean f=false;
+        while(!f) {
+            System.out.println("Введите длину в метрах");
+            m=sc.nextInt();
+            System.out.println("Введите длину в сантиметрах");
+            sm=sc.nextInt();
+            try {
+                if (m < 0 || sm < 0)
+                    throw new IOException("Длина не может быть отрицательной!");
+                f = true;
+            } catch (IOException exc) {
+                System.out.println(exc.getMessage());
+            }
         }
-        System.out.println("Your number is "+right);
+        double feet=0;
+        double dm=0;
+        sm+=m*100;
+        sm=sm/2.54;
+        dm=sm;
+        feet=(int)(dm/12);
+        dm-=feet*12;
+        System.out.println("Высота составляет "+feet+" фут и "+dm+" инчей");
+
     }
-    public static String arr(){
-        return "xuy v rot";
+    static boolean check(String s){
+        try{
+            Integer.parseInt(s);
+        }
+        catch (Exception ex){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public static void reverse(int[] mas, int l){
+        for(int i=0;i<l/2;i++){
+            int t=mas[l-i-1];
+            mas[l-i-1]=mas[i];
+            mas[i]=t;
+        }
     }
 }
---
+++ src/anna.java
@@ -0,0 +1,5 @@
+/**
+@author ivan
+ */
+public @interface anna {
+}
